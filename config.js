a04138653896938a429779121ed21d268139f22f
Owain Roberts <owain.roberts@bjss.com>
2019-10-10 10:40:18 +0100

    EE-26903 Change default template IDs to be the ones in the Euro TLR service

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ module.exports = {
         apiKey: process.env.NOTIFY_API_KEY || 'callbacktest-9eafa895-b7f4-487f-b4fa-8cb3a6796132-817b0178-02ff-45e9-9da2-df251b9c79b2',
         templates: {
             organisation: {
-                general: process.env.NOTIFY_TEMPLATE_ORG_GENERAL || '4fdc0fd3-31f6-447c-9184-ddfcd5042f42',
-                application: process.env.NOTIFY_TEMPLATE_ORG_APP || '6e20e03e-0fff-4202-be11-0478aa502b42'
+                general: process.env.NOTIFY_TEMPLATE_ORG_GENERAL || 'a9d8c4b9-d315-4d65-a5d1-619a32169930',
+                application: process.env.NOTIFY_TEMPLATE_ORG_APP || '71baf484-76e7-4ee1-ae1c-18d8d56d152e'
             },
             individual: {
-                general: process.env.NOTIFY_TEMPLATE_IND_GENERAL || '9867fbb4-3c0c-4377-a57c-be2929e2e2f6',
-                application: process.env.NOTIFY_TEMPLATE_IND_APP || '85ea4e91-ecaf-4447-b6dc-1cfd245d8dbc'
+                general: process.env.NOTIFY_TEMPLATE_IND_GENERAL || '6bf9f478-41c1-4d1f-b9ea-4aef4b84578e',
+                application: process.env.NOTIFY_TEMPLATE_IND_APP || '5d1ba7f1-f9a6-4d54-bae7-367bd4252033'
             }
         },
         recipient: process.env.NOTIFY_RECIPIENT || 'simulate-delivered@notifications.service.gov.uk'
